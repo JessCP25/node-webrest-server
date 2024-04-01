f22b13aad53311542309668f4a8a850c2feb1a04
JessCP25 <jpayanoc25@gmail.com>
2024-04-01 17:36:14 -0500

    Diferentes respuestas

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
 import http from 'http';
+import fs from 'fs';
 
 const server = http.createServer((req, res)=>{
   console.log(req.url);
 
-  res.write('Hola mundo');
+  // res.writeHead(200, {'Content-Type': 'text/html'});
+  // res.write(`<h1>Hola ${req.url}</h1>`)
+  // res.end();
 
-  res.end();
+  // const user = {name: 'John Doe', age: 32, city: 'New York'};
+  // res.writeHead(200, {'Content-type': 'application/json'});
+  // res.end(JSON.stringify(user));
+
+  if(req.url === '/'){
+    const data = fs.readFileSync('./public/index.html', 'utf-8')
+    res.writeHead(200, {'Content-type':  'text/html'});
+    res.end(data)
+  }else{
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.end();
+  }
+
 })
 
 server.listen(3000, ()=>{
