5a1090a28e71601fb5ac5bad3f4688b854408ce0
JessCP25 <jpayanoc25@gmail.com>
2024-04-02 12:23:12 -0500

    Servir SPA con Router

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 export class Server {
   private app = express();
@@ -9,6 +10,11 @@ export class Server {
     //* PublicFolder
 
     this.app.use(express.static('public'));
+
+    this.app.get('*', (req, res)=>{
+      const filePath = path.join(__dirname, '../../public/index.html')
+      res.sendFile(filePath);
+    })
 
     this.app.listen(3000, ()=>{
       console.log(`Server running in port ${3000}`)
