47a16fedebed481867dcbbb6ec4aa4c50605cce3
JessCP25 <jpayanoc25@gmail.com>
2024-04-02 15:04:39 -0500

    Varibles dee entorno

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,23 +1,36 @@
 import express from 'express';
 import path from 'path';
 
+export interface Options {
+  port: number,
+  public_path?: string,
+}
+
 export class Server {
   private app = express();
+  private port: number;
+  private public_path: string;
+
+  constructor(private options: Options){
+    const {port, public_path = 'public'} = options;
+    this.port = port;
+    this.public_path = public_path;
+  }
 
   async start(){
     //* Middlewares
 
     //* PublicFolder
 
-    this.app.use(express.static('public'));
+    this.app.use(express.static(this.public_path));
 
     this.app.get('*', (req, res)=>{
-      const filePath = path.join(__dirname, '../../public/index.html')
+      const filePath = path.join(__dirname, `../../${this.public_path}/index.html`)
       res.sendFile(filePath);
     })
 
-    this.app.listen(3000, ()=>{
-      console.log(`Server running in port ${3000}`)
+    this.app.listen(this.port, ()=>{
+      console.log(`Server running in port ${this.port}`)
     })
   }
 }
