9f8fb1b13d69a30b3b0798996c5b73dc3b3bd09b
JessCP25 <jpayanoc25@gmail.com>
2024-04-03 00:05:11 -0500

    Agregando controllers

---
+++ src/presentation/todos/controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+export class TodosController {
+  public getTodos = (req: Request, res: Response) => {
+    return res.json([
+      { id: 1, text: "Buy milk", createdAt: new Date() },
+      { id: 2, text: "Buy bread", createdAt: null },
+      { id: 3, text: "Buy butter", createdAt: new Date() },
+    ]);
+  };
+}
