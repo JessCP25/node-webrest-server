b27cad80e0e7c5916dee859141cac242255f3a18
JessCP25 <jpayanoc25@gmail.com>
2024-04-04 22:53:10 -0500

    Crud actualizar

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 
 const todos = [
-  { id: 1, text: "Buy milk", createdAt: new Date() },
-  { id: 2, text: "Buy bread", createdAt: null },
-  { id: 3, text: "Buy butter", createdAt: new Date() },
+  { id: 1, text: "Buy milk", completedAt: new Date() },
+  { id: 2, text: "Buy bread", completedAt: null },
+  { id: 3, text: "Buy butter", completedAt: new Date() },
 ];
 
 export class TodosController {
@@ -23,15 +23,31 @@ export class TodosController {
   };
 
   public createTodo = (req: Request, res: Response) => {
-    const {text} = req.body;
-    if(!text) return res.status(400).json({error: "Text is required"});
+    const { text } = req.body;
+    if (!text) return res.status(400).json({ error: "Text is required" });
 
     const newTodo = {
-      id: todos.length +1,
+      id: todos.length + 1,
       text,
-      createdAt: null
-    }
+      completedAt: null,
+    };
     todos.push(newTodo);
     return res.json(newTodo);
+  };
+
+  public updateTodo = (req: Request, res: Response) => {
+    const id = +req.params.id;
+    if (isNaN(id))
+      return res.status(400).json({ error: "ID argument is not a number" });
+    const todo = todos.find((todo) => todo.id === id);
+    if (!todo)
+      return res.status(404).json({ error: `Todo with id ${id} is not found` });
+    const { text, completedAt } = req.body;
+    todo.text = text || todo.text;
+    completedAt === "null"
+      ? (todo.completedAt = null)
+      : (todo.completedAt = new Date(completedAt || todo.completedAt));
+
+    return res.json(todo);
   };
 }
